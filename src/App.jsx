936f15e7fdaa1cfce2d6d15dfeaf63a826806224
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-12 17:45:37 +0100

    Print a list of both actresses and actors, separately.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { useState, useEffect } from "react"
 
 function App() {
   const [listAttrici, setListAttrici] = useState([]);
+  const [listAttori, setListAttori] = useState([]);
   const api = "https://lanciweb.github.io/demo/api/actresses/";
+  const api2 = "https://lanciweb.github.io/demo/api/actors/";
 
   useEffect(fetchGenera, [])
+  useEffect(fetchGeneraAttori, [])
 
   function fetchGenera() {
     axios
@@ -16,19 +19,26 @@ function App() {
       })
   }
 
+  function fetchGeneraAttori() {
+    axios
+      .get(api2)
+      .then(res => {
+        setListAttori(res.data);
+        console.log(res.data);
+      })
+  }
+
   return (
     <>
-      <header className="py-3 text-center">
-        <h1>Attrici</h1>
-      </header>
-
       <main>
         <div className="container">
-          <div className="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+          <h1 className="py-3 text-center">Attrici</h1>
+          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 py-5">
+
             {
               listAttrici.map((attrice) => (
                 <div key={attrice.id} className="col">
-                  <div className="card-content">
+                  <div className="card-content-attrici">
                     <img src={attrice.image} alt="" className="card-img-top" />
                     <div className="card-body">
                       <h3>{attrice.name}</h3>
@@ -43,14 +53,30 @@ function App() {
             }
           </div>
         </div>
+
+        <div className="container">
+          <h1 className="py-3 text-center">Attori</h1>
+          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 py-5">
+            {
+              listAttori.map((attore) => (
+                <div key={attore.id} className="col">
+                  <div className="card-content-attori">
+                    <img src={attore.image} alt="" className="card-img-top" />
+                    <div className="card-body">
+                      <h3>{attore.name}</h3>
+                      <span><strong>Biography:</strong> {attore.biography}</span>
+                      <span><strong>Birth year:</strong> {attore.birth_year}</span>
+                      <span><strong>Nationality:</strong> {attore.nationality}</span>
+                      <span><strong>Awards:</strong> {attore.awards}</span>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </main >
     </>
   )
 }
 export default App
-
-{/*
-  MILESTONE 2
-
-Prepariamo una card per ciascun attore/attrice, mostrandone le seguenti informazioni:
-*/}
