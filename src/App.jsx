83c8ae179f4dff4498da9d18911422ff38654e69
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-12 12:49:52 +0100

    retrieve list of actresses from the API and print to  console

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 
 function App() {
+  const [listAttrici, setListAttrici] = useState([]);
+  const api = "https://lanciweb.github.io/demo/api/actresses/";
+
+  useEffect(fetchGenera, [])
+
+  function fetchGenera() {
+    axios
+      .get(api)
+      .then(res => {
+        console.log(res.data);
+        setListAttrici(res.data);
+      })
+  }
 
   return (
     <>
+      <h1>Attrici</h1>
 
     </>
   )
 }
-
 export default App
+
