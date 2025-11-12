24ad6f78e34cead447396c90f635a0782a6b8026
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-12 13:43:26 +0100

    add a card for each actress, showing the information

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,40 @@ function App() {
     axios
       .get(api)
       .then(res => {
-        console.log(res.data);
         setListAttrici(res.data);
+        console.log(res.data);
       })
   }
 
   return (
     <>
-      <h1>Attrici</h1>
+      <header>
+        <h1>Attrici</h1>
+      </header>
 
+      <main>
+        <div className="card-container">
+          {
+            listAttrici.map((attrice) => (
+              <div key={attrice.id} className="card-content mb-4 bg-warning">
+                <img src={attrice.image} alt="" />
+                <h4>{attrice.name}</h4>
+                <p>{attrice.birth_year}</p>
+                <p>{attrice.nationality}</p>
+                <p>{attrice.biography}</p>
+                <p>{attrice.awards}</p>
+              </div>
+            ))
+          }
+        </div>
+      </main >
     </>
   )
 }
 export default App
 
+{/*
+  MILESTONE 2
+
+Prepariamo una card per ciascun attore/attrice, mostrandone le seguenti informazioni:
+*/}
