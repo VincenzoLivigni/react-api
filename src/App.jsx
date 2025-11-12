8f2fc6f106a416bf26bdd7b19a9a41d388d2d054
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-12 15:26:51 +0100

    add style to card for each actor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 
-
 function App() {
   const [listAttrici, setListAttrici] = useState([]);
   const api = "https://lanciweb.github.io/demo/api/actresses/";
@@ -19,24 +18,30 @@ function App() {
 
   return (
     <>
-      <header>
+      <header className="py-3 text-center">
         <h1>Attrici</h1>
       </header>
 
       <main>
-        <div className="card-container">
-          {
-            listAttrici.map((attrice) => (
-              <div key={attrice.id} className="card-content mb-4 bg-warning">
-                <img src={attrice.image} alt="" />
-                <h4>{attrice.name}</h4>
-                <p>{attrice.birth_year}</p>
-                <p>{attrice.nationality}</p>
-                <p>{attrice.biography}</p>
-                <p>{attrice.awards}</p>
-              </div>
-            ))
-          }
+        <div className="container">
+          <div className="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+            {
+              listAttrici.map((attrice) => (
+                <div key={attrice.id} className="col">
+                  <div className="card-content">
+                    <img src={attrice.image} alt="" className="card-img-top" />
+                    <div className="card-body">
+                      <h3>{attrice.name}</h3>
+                      <span><strong>Biography:</strong> {attrice.biography}</span>
+                      <span><strong>Birth year:</strong> {attrice.birth_year}</span>
+                      <span><strong>Nationality:</strong> {attrice.nationality}</span>
+                      <span><strong>Awards:</strong> {attrice.awards}</span>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
         </div>
       </main >
     </>
